0b1fb29917ee6850cb71e11bd5193cc2aaa41507
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-01-21 17:55:10 +0100

    acabado

--- src/ListaTareasPrioridad.java
+++ src/ListaTareasPrioridad.java
@@ -1,13 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class ListaTareasPrioridad {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> tareas = new ArrayList<>();
+        int opcion = -1;
+        String tarea;
+        int posicion;
+
+        do {
+            try {
+                System.out.println("Elija una opción");
+                System.out.println("1. Añadir una tarea al final");
+                System.out.println("2. Insertara una tarea en una posición concreta");
+                System.out.println("3. Eliminar una tarea por posición");
+                System.out.println("4. Mostrar todas las tareas numeradas");
+                System.out.println("0. Salir");
+                opcion = sc.nextInt();
+                sc.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println("\uD83D\uDDF6 Elija una opción correcta");
+                sc.nextLine();
+            }
+
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Indique la tarea que desea añadir");
+                    tarea = sc.nextLine();
+                    tareas.add(tarea);
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Indique la tarea que desea añadir");
+                    tarea = sc.nextLine();
+                    System.out.println("Indique la posición donde desea añadir la tarea");
+                    posicion = sc.nextInt();
+                    sc.nextLine();
+                    tareas.add(posicion, tarea);
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Indique la posición de la tarea que desea eliminar");
+                    posicion = sc.nextInt();
+                    sc.nextLine();
+                    tareas.remove(posicion);
+                    System.out.println();
+                    break;
+                case 4:
+                    for(int i = 0; i < tareas.size(); i++){
+                        System.out.println(i + ". " + tareas.get(i));
+                    }
+                    System.out.println();
+                    break;
+                case 0:
+                    System.out.println("Hasta pronto!!!");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    System.out.println();
+                    break;
+            }
+        } while (opcion != 0);
+
+    }
 }
